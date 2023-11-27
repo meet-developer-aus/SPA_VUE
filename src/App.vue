a43104da39b4cd01b218561d7f9d1a91f4453903
romeoantony53 <romeorealist@gmail.com>
2023-11-27 04:30:09 +0000

    Modeal overlay component skelton added and working without accommodation options

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+   
+
+   
+
+    <Modal @closeModal="closeModal">
+      
+     
+    </Modal>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import  Modal from  './components/Modal.vue'
+import { ref } from 'vue'
 
 export default {
-  name: 'App',
+   name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Modal,
+  },
+  setup()
+  {
 
+
+const closeModal=() =>{
+      // Handle modal close if needed
+    }
+
+    return {closeModal}
+  }
+  
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Modal.vue
@@ -0,0 +1,105 @@
+<template>
+
+<div>This is a modal</div><div class="modal-overlay" v-if="isVisible">
+    <div class="modal">
+      <div class="modal-header">
+        <h2>{{ title }}</h2>
+        <button @click="closeModal">&times;</button>
+      </div>
+      <div class="modal-content">
+        <!-- Your modal content goes here -->
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'Modal',
+  setup()
+  {
+
+    let  isVisible= ref(true);
+    const title=ref('Accommodation Options in Sydney');
+
+//  const openModal=() =>{
+//       isVisible.value = true;
+//     }
+
+    const closeModal=() =>{
+      isVisible.value= false;
+    }
+
+    return {
+      isVisible, title,closeModal
+      
+    }
+  
+    
+  }
+
+  
+  
+}
+</script>
+
+<style scoped>
+/* Modal Overlay styless*/
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+}
+
+/* Modal styless*/
+.modal {
+  background: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  width: 600px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+/* Modal Header */
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 10px;
+}
+
+.modal-header h2 {
+  margin: 0;
+}
+
+/* Close Button */
+.close-btn {
+  background: none;
+  border: none;
+  font-size: 1.5rem;
+  cursor: pointer;
+  color: #555;
+}
+
+.close-btn:hover {
+  color: #333;
+}
+
+/* Modal Content */
+.modal-content {
+  /* Add your content styling here */
+}
+
+</style>
